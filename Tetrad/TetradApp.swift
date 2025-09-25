3ebe115ab5910355b081e1dbf1f5a10cb4d4e33b
Kevin Nations <mrnations@gmail.com>
2025-09-24 22:53:27 -0500

    Initial Commit

---
+++ Tetrad/TetradApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetradApp.swift
+//  Tetrad
+//
+//  Created by kevin nations on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TetradApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
